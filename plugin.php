f7a4fcd17fe6a25500780ba330f5de86ca36bd6f
Dovy Paukstys <dovy@rtcollab.com>
2013-08-25 11:34:21 -0600

    Version push.

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Simple Options Framework
  * Plugin URI: https://github.com/SimpleRain/SimpleOptions
  * Description: A simple wordpress options framework for developers.
- * Version: 0.2.2
+ * Version: 0.2.4
  * Author: SimpleRain
  * Author URI: http://simplerain.com
  *
